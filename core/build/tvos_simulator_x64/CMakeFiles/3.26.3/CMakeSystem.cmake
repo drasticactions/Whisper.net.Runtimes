9e16586087966d6deaeea46e15285d0ce465abf0
Tim Miller <innerlogic4321@gmail.com>
2023-05-17 00:01:25 +0900

    Add CoreML

---
+++ core/build/tvos_simulator_x64/CMakeFiles/3.26.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Darwin-22.4.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "22.4.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
+
+include("/Users/drasticactions/Developer/Personal/Libraries/Whisper.net.Runtimes/ios.toolchain.cmake")
+
+set(CMAKE_SYSTEM "tvOS-16.4")
+set(CMAKE_SYSTEM_NAME "tvOS")
+set(CMAKE_SYSTEM_VERSION "16.4")
+set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
